ed83f71cfc97698e1c773db2493211ac5beb8b5a
stefan-werner <stefan.werner@infai.org>
2022-02-04 12:25:07 +0100

    use spaw method for torch on cuda

--- service.py
+++ service.py
@@ -3,6 +3,9 @@ from cdqa.utils.converters import pdf_converter
 from cdqa.utils.filters import filter_paragraphs
 from cdqa.pipeline import QAPipeline
 
+if "torch_spawn" in os.environ["gpu"]:
+    import torch
+    torch.multiprocessing.set_start_method('spawn')
 
 """
 Preparation
